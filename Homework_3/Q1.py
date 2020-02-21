a85d733b12cf0159ce1594cf2e77dd278ac14cf4
Matt Richard <mattrichard@nevada.unr.edu>
2020-02-21 02:02:37 -0800

    HW3_Q1 updates

--- Homework_3/Q1.py
+++ Homework_3/Q1.py
@@ -1,39 +1,69 @@
 import matplotlib.pyplot as plt
 import csv
 import numpy as np
-from scipy.optimize import fsolve
 
 F = 0.085   # m^3 / min
 V = 2.1     # m^3
 k = 0.5     # m^3 / (mol * min)
-Caos = 1.5  # mol / m^3
-# Cas = 0.5   # mol / m^3
 
-def tau(Cas):   # min
-    y = V / (F + 2 * V * k * Cas)
+Cao_init = 0.925    # mol / m^3         (assumed value)
+Cao_prime = 1       # mol / m^3
+Cao = Cao_init + Cao_prime  # mol / m^3
+
+Ca_init = 0.236     # mol / m^3         (assumed value)
+
+
+#Part A; Analytical Solution:
+tau = V / (F + 2*V*k*Ca_init)
+kp = F / (F + 2*V*k*Ca_init)
+
+def Ca_dev_analyt(t):
+    y = Cao_prime * kp * (1 - np.exp(-t / tau))
     return y
-def kp(Cas):    # unitless
-    y = F / (F + 2 * V * k * Cas)
+
+
+#Part B; Numerical Solution & Graphical Comparison:
+def dCa_dt(Ca):
+    y = F / V * (Cao - Ca) - k * Ca**2
     return y
 
-
-def Ca_analyt(t, Cao_prime, Cas):
-    y = Cao_prime * kp(Cas) * (1 - np.exp(-t/tau(Cas)))
-    return y
-
-def part_a_ans(step, Cao_prime, Cas):
-    error = 1.0
-    tol = 1e-10
+def Euler(step):
     t = 1.0
-    Ca_prime = 0
-    while error >= abs(tol):
-        Ca_prime_new = Ca_analyt(t, Cao_prime, Cas)
+    Ca_prime_a = 0  # Ca_prime_a is a deviation variable of Ca for analyt soln
+    Ca_prime_n = 0  # Ca_prime_n is a deviation variable of Ca for num soln
+    tol = 1e-4
+    error_a = 1
+    error_n = 1
+    time_list_a = [t]
+    time_list_n = [t]
+    analyt_list = [Ca_prime_a]
+    numerical_list = [Ca_prime_n]
+
+    while error_a >= abs(tol):
+        analyt_new = Ca_dev_analyt(t)
+        error_a = abs(analyt_new - Ca_prime_a) / analyt_new
+        Ca_prime_a = analyt_new
         t = t + step
-        error = abs(Ca_prime_new - Ca_prime) / Ca_prime_new
-        Ca_prime = Ca_prime_new
-    print(t, Ca_prime)
-    return t, Ca_prime
+        time_list_a.append(t)
+        analyt_list.append(Ca_prime_a)
+    print("Time to reach steady state for analytical:", t, "minutes")
 
+    t = 1.0
+    while error_n >= abs(tol):
+        num_new = Ca_prime_n + dCa_dt(Ca_prime_n) * step
+        error_n = abs(num_new - Ca_prime_n) / num_new
+        Ca_prime_n = num_new
+        t = t + step
+        time_list_n.append(t)
+        numerical_list.append(Ca_prime_n)
+    print("Time to reach steady state for numerical:", t, "minutes")
 
-for Cas in (0.1, 0.5, 0.75):
-    part_a_ans(0.1, 1, Cas)
+    return [time_list_a, time_list_n, analyt_list, numerical_list]
+
+steadystate = Euler(0.1)
+
+with open('Q1ans.csv', 'w', newline='') as ans:
+    wr = csv.writer(ans)
+    wr.writerow(["Time (minutes)", "Ca_analytical approx (mol / m^3)", "Ca_numerical approx (mol / m^3)"])
+    for i in range(len(steadystate[0])):
+        wr.writerow([steadystate[0][i], steadystate[2][i], steadystate[3][i]])
