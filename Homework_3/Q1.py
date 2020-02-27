44251b8f7af4ba3a0727538cbe6e1ff64604cbc3
Matt Richard <mattrichard@nevada.unr.edu>
2020-02-27 06:18:54 -0800

    HW3

--- Homework_3/Q1.py
+++ Homework_3/Q1.py
@@ -6,75 +6,103 @@ F = 0.085   # m^3 / min
 V = 2.1     # m^3
 k = 0.5     # m^3 / (mol * min)
 
-Cao_init = 0.925        # mol / m^3         (assumed value)
-Cao_prime = 1       # mol / m^3
-Cao = Cao_init + Cao_prime  # mol / m^3
-
-Ca_init = 0.236         # mol / m^3         (assumed value)
+Cao_s = 1       # mol / m^3
+Cao_prime = 1   # mol / m^3
+Ca_s = 0.1        # mol / m^3
 
 
 #Part A; Analytical Solution:
-tau = V / (F + 2*V*k*Ca_init)
-kp = F / (F + 2*V*k*Ca_init)
+def tau(F):
+    y = V / (F + 2*V*k*Ca_s)
+    return y
+def kp(F):
+    y = F / (F + 2*V*k*Ca_s)
+    return y
 
-def Ca_dev_analyt(t):
-    y = Cao_prime * kp * (1 - np.exp(-t / tau))
+def Ca_prime_analyt(t, F):
+    y = Cao_prime * kp(F) * (1 - np.exp(-t / tau(F)))
     return y
 
 
 #Part B; Numerical Solution & Graphical Comparison:
-def dCa_dt(Ca):
-    y = F / V * (Cao_prime - (Ca - Ca_init)) - k * (Ca**2 + Ca_init**2)
+def dCa_prime_dt(Ca, F):
+    y = F / V * (Cao_prime - (Ca - Ca_s)) - k * (Ca**2 - Ca_s**2)
     return y
 
-def Euler(step):
-    t = 0.01
-    Ca_prime_a = 0  # Ca_prime_a is a deviation variable of Ca for analyt soln
-    Ca_prime_n = 0  # Ca_prime_n is a deviation variable of Ca for num soln
+def steady_state(step, F):
     tol = 1e-5
     error = 1
+
+    t = 0
+    tau_counter = t / tau(F)
+    Ca_prime_a = 0
+    Ca_prime_e = 0
+
+    tau_list = [tau_counter]
     time_list = [t]
-    analyt_list = [Ca_prime_a]
-    numerical_list = [Ca_prime_n]
+    Ca_prime_a_list = [Ca_prime_a]
+    Ca_prime_e_list = [Ca_prime_e]
     Cao_prime_list = [0]
+    diff_list = [0]
 
     while error >= abs(tol):
-        # Feed data:
-        Cao_prime_list.append(Cao_prime)
+        # Analytical Method:
+        Ca_prime_a_new = Ca_prime_analyt(t, F)
+        error_a = abs(Ca_prime_a_new - Ca_prime_a) / Ca_prime_a_new
+        if t == 0:
+            error_a = 1
+        Ca_prime_a = Ca_prime_a_new
+        Ca_prime_a_list.append(Ca_prime_a)
 
-        # Analytical loop:
-        analyt_new = Ca_dev_analyt(t)
-        error_a = abs(analyt_new - Ca_prime_a) / analyt_new
-        Ca_prime_a = analyt_new
-        analyt_list.append(Ca_prime_a)
+        # Euler's Method:
+        Ca_prime_e_new = Ca_prime_e + dCa_prime_dt(Ca_prime_e, F) * step
+        error_e = abs(Ca_prime_e_new - Ca_prime_e) / Ca_prime_e_new
+        if t == 0:
+            error_e = 1
+        Ca_prime_e = Ca_prime_e_new
+        Ca_prime_e_list.append(Ca_prime_e)
 
-        #Numerical loop:
-        num_new = Ca_prime_n + dCa_dt(Ca_prime_n) * step
-        error_n = abs(num_new - Ca_prime_n) / num_new
-        Ca_prime_n = num_new
-        numerical_list.append(Ca_prime_n)
-        error = (error_a + error_n) / 2
-
-        time_list.append(t)
+        # Time Iterations
         t = t + step
-    print("Steady state for analytical method:", t, "minutes", ",", Ca_prime_a, "mol / m^3")
-    print("Steady state for numerical method:", t, "minutes", ",", Ca_prime_n, "mol / m^3")
-    return [time_list, analyt_list, numerical_list, Cao_prime_list]
+        time_list.append(t)
+        tau_counter = t / tau(F)
+        tau_list.append(tau_counter)
 
-
-steadystate = Euler(0.1)
+        # Other:
+        Cao_prime_list.append(Cao_prime)
+        difference = abs(Ca_prime_e - Ca_prime_a)
+        diff_list.append(difference)
+        error = (error_a + error_e) / 2
+    return(tau_list, time_list, Ca_prime_a_list, Ca_prime_e_list, Cao_prime_list, diff_list,
+           tau_counter, t, Ca_prime_a, Ca_prime_e, difference)
 
 with open('Q1ans.csv', 'w', newline='') as ans:
     wr = csv.writer(ans)
-    wr.writerow(["Time (minutes)", "Ca_analytical approx (mol / m^3)", "Ca_numerical approx (mol / m^3)"])
-    for i in range(len(steadystate[0])):
-        wr.writerow([steadystate[0][i], steadystate[1][i], steadystate[2][i]])
+    wr.writerow(["Flow Rate (m^3 / min)", "Tau's", "Time (min)", "Ca'_analyt", "Ca'_euler", "difference in Ca'from models"])
+
+test_range = [0.085, 1e-5, 0.01, 0.1, 0.25, 0.5, 1, 2, 5, 10]
+for Flow in (test_range):
+    compare = steady_state(0.1, Flow)
+
+    with open('Q1ans.csv', 'a', newline='') as ans:
+        wr = csv.writer(ans)
+        wr.writerow([Flow, compare[6], compare[7], compare[8], compare[9], compare[10]])
+    print("With a Flow Rate of:", Flow, "m^3 / min:")
+    print("Analytical method concentration:", compare[8], "mol / m^3")
+    print("Numerical method concentraction:", compare[9], "mol / m^3")
+    print("Difference in methods:", compare[10], "mol / m^3")
+    plt.plot(compare[0], compare[5], label=Flow)
+plt.xlabel("Tau (min)")
+plt.ylabel("Difference between modelling methods")
+plt.legend(title="Flow Rates (m^3 / min):")
+plt.show()
 
 
+ans = steady_state(0.1, F)
 
-plt.plot(steadystate[0], steadystate[1], label="Analytical model", color="k")
-plt.plot(steadystate[0], steadystate[2], label="Numerical model", color="g")
-plt.plot(steadystate[0], steadystate[3], label="Feed Stream", color="b")
+plt.plot(ans[1], ans[2], label="Analytical model", color="k")
+plt.plot(ans[1], ans[3], label="Numerical model", color="g")
+plt.plot(ans[1], ans[4], label="Feed Stream", color="b")
 plt.legend(title="Modeling Method")
 plt.xlabel("Time (min)")
 plt.ylabel("Deviation Concentration of A (mol / m^3)")
--- Lab/Heater_characterization_data.py
+++ Lab/Heater_characterization_data.py
@@ -1,9 +1,57 @@
 import csv
+import numpy as np
 
-with open("Temp_Data.csv", "r", newline='') as data:
+with open("New_Bed_Height.csv", "r", newline='') as data:
     rd = csv.reader(data, delimiter='\t')
-    print(
+    next(rd)
+    next(rd)
 
-with open('Averaged_Data.csv', 'w', newline='') as avg:
-    wr = csv.writer(avg)
-    wr.writerow(["Time(sec)",	"Level",	"Setpoint",	"%Valve Output",	"Temp.",	"Setpoint",	"%Power to Heater"])
+    time_list = []
+    level_list = []
+    temp_list = []
+    power_to_heater_list = []
+
+    data_points = 0
+    time = 0
+    while data_points < 5e3:
+        counter = 0
+        level_data = []
+        temp_data = []
+        power_data = []
+        for row in rd:
+            if counter > 9:
+                time_list.append(time)
+                time += 1
+                level_list.append(level_avg)
+                temp_list.append(temp_avg)
+                power_to_heater_list.append(heater_avg)
+                break
+
+            level_numbers = float(row[1])
+            level_data.append(level_numbers)
+            level_avg = np.average(level_data)
+
+            temp_numbers = float(row[4])
+            temp_data.append(temp_numbers)
+            temp_avg = np.average(temp_data)
+
+            heater_numbers = float(row[6])
+            power_data.append(heater_numbers)
+            heater_avg = np.average(power_data)
+
+            counter += 1
+        data_points += 1
+
+
+
+
+    with open('Averaged_Data.csv', 'w', newline='') as avg:
+        wr = csv.writer(avg)
+        wr.writerow(["Time(sec)", "Level (cm)", "Temp. (*C)", "% Power to Heater"])
+        for i in range(len(time_list)):
+            wr.writerow([time_list[i], level_list[i], temp_list[i], power_to_heater_list[i]])
+
+
+# print("Time:", time_list)
+# print("Temp:", temp_list)
+# print("Level:", level_list)
